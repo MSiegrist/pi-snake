a333f08b31d76f02049a542a23527e107e84f1fd
joseph <josephgallo97@gmail.com>
2023-04-04 14:30:14 +0200

    implemented first batch of ideas for GameState Class

--- App/GameState.cs
+++ App/GameState.cs
@@ -6,6 +6,11 @@ using System.Threading.Tasks;
 
 namespace App
 {
+    /*
+    The GameState class represents the current state of the game and keeps
+    track of the playfield, score and gameOver State (bool) 
+    */
+
     internal class GameState
     {
         public GameTile[,] Playfield { get; }
@@ -14,6 +19,58 @@ namespace App
 
         public bool GameOver { get; private set; }
 
-        public GameState() { }
+        public GameTile.Snake[,]
+
+        public GameTile.Fruit 
+
+        //Maybe add difficulty to GameState(int width, int length, bool difficultyEasy)? 
+        //public bool DifficultEasy { get; private set; }
+
+
+        //GameTile Size score and GameOverState will be initialized here 
+        //this method will be called in SnakeGame Class
+        public GameState(int width, int length)
+        {
+            Playfield = new GameTile[width, length];
+            Score = 0;
+            GameOver = false;
+            //DifficultyEasy = difficultyEasy
+
+            //Set initial Snake Position? --> Place it always in the middle
+            int initialSnakeX = width / 2;
+            int initialSnakeY = length / 2;
+            GameTile.Snake snake = new GameTile.Snake[initialSnakeX, initialSnakeY];
+
+            //Set First Fruit
+            GameTile.Fruit = GenerateInitialFruit(width, length);
+
+
+        }
+
+        public void IncreaseScore()
+        {
+            Score++;
+        }
+
+        public void SetGameOverState()
+        {
+            GameOver = true;
+        }
+
+        public Fruit GenerateInitialFruit(int width, int length)
+        {
+            int fruitX;
+            int fruitY;
+
+            do
+            {
+                fruitX = new Random().Next(0, width);
+                fruitY = new Random().Next(0, length);
+                Fruit firstFruit = new GameTile.Fruit[fruitX, fruitY];
+
+            } while (snake != firstFruit);
+
+
+        }
     }
 }
